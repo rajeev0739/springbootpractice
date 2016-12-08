315e9368388cb659778dbfdc0bc7b63147c14271
rajeevkr <rajeev.kumar@vuclip.com>
2016-12-08 12:43:07 +0530

    Commiting after Class Rename

--- src/main/java/com/rajeev/sbp/SpringBootPracticeApplication.java
+++ src/main/java/com/rajeev/sbp/SpringBootPracticeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootpracticeApplication {
+public class SpringBootPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootpracticeApplication.class, args);
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
 	}
 }
--- src/test/java/com/rajeev/sbp/SpringBootPracticeApplicationTests.java
+++ src/test/java/com/rajeev/sbp/SpringBootPracticeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootpracticeApplicationTests {
+public class SpringBootPracticeApplicationTests {
 
 	@Test
 	public void contextLoads() {
