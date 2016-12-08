980c6b011a189d8c093da0c5b2218fcb9a977ae1
rajeev0739 <rajeev0739@gmal.com>
2016-12-08 20:25:17 +0530

    Adding the spring boot based file logging

--- src/main/java/com/rajeev/sbp/SpringBootPracticeApplication.java
+++ src/main/java/com/rajeev/sbp/SpringBootPracticeApplication.java
@@ -2,6 +2,8 @@ package com.rajeev.sbp;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,21 +12,28 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootPracticeApplication {
+	 private static final Logger logger = LoggerFactory.getLogger(SpringBootPracticeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticeApplication.class, args);
+		
+		   logger.error("Message logged at ERROR level");
+		   logger.warn("Message logged at WARN level");
+		   logger.info("Message logged at INFO level");
+		   logger.debug("Message logged at DEBUG level");
+
 	}
 	
 	@Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+        	logger.debug("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+            	logger.debug(beanName);
             }
 
         };
--- src/main/java/com/rajeev/sbp/controller/HelloController.java
+++ src/main/java/com/rajeev/sbp/controller/HelloController.java
@@ -2,14 +2,17 @@ package com.rajeev.sbp.controller;
 
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/")
     public String index() {
+    	logger.debug("Inside index::");
         return "Greetings from Rajeev's Spring Boot App !!";
     }
 
