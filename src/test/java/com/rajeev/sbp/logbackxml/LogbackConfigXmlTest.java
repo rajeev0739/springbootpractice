3f15098d6986c4bf702a260712a8ed7c9c1ff022
rajeev0739 <rajeev0739@gmal.com>
2016-12-09 20:30:34 +0530

    commiting after logback related logging change

---
+++ src/test/java/com/rajeev/sbp/logbackxml/LogbackConfigXmlTest.java
@@ -0,0 +1,14 @@
+package com.rajeev.sbp.logbackxml;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+ 
+ 
+public class LogbackConfigXmlTest {
+    @Test
+    public void testPerformTask() throws Exception {
+        LogbackConfigXml logbackConfigXml=new LogbackConfigXml();
+        logbackConfigXml.performTask();
+    }
+}
